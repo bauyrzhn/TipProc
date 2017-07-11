a7dc00ad35743219d9498d7695f3869dde53e033
Bauyrzhan Orynbassar <bauyrzhn@gmail.com>
2017-07-11 16:53:13 -0700

    Initial Commit

---
+++ TipProc/File.swift
@@ -0,0 +1,11 @@
+//
+//  File.swift
+//  TipProc
+//
+//  Created by Bauyrzhan on 7/5/17.
+//  Copyright © 2017 Make School. All rights reserved.
+//
+
+import Foundation
+
+class MyOtherViewController
